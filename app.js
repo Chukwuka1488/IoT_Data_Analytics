5ff5085076647ef258bbe5e41328eed3ba860732
Chukwuka1488 <juniorakibor@yahoo.com>
2021-06-05 23:39:27 +0200

    created add campground form file for server side

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+//middleware
+// needed when we want to post a data
+app.use(express.urlencoded({ extended: true }));
+
 // creating new data or products
 app.get("/", (req, res) => {
   res.render("home");
@@ -33,12 +37,25 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+// create the render form file route for the client side
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
 // create the show file route for the client side
 app.get("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   res.render("campgrounds/show", { campground });
 });
+
+app.post("/campgrounds", async (req, res) => {
+  // res.send(req.body);
+  const newCampground = new Campground(req.body.campground);
+  await newCampground.save();
+  res.redirect(`/campgrounds/${newCampground._id}`);
+});
+
 // localhost
 app.listen(3005, () => {
   console.log("APP IS LISTENING ON PORT 3005");
