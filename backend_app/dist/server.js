6fdd5a454c4748ef8425b499f3a059f9b3f1fbc0
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-07-08 18:35:34 +0200

    backend update to prevent CORS error

--- backend_app/dist/server.js
+++ backend_app/dist/server.js
@@ -4,10 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = 3500;
+app.use((0, cors_1.default)({
+    origin: "http://localhost:3000"
+}));
 app.get("/", (req, res) => {
     res.send("Hello, World!");
+    // res.json({"users": ['one', 'two', 'three', 'four']});
 });
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
--- backend_app/src/server.ts
+++ backend_app/src/server.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3500;
 
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
+  // res.json({"users": ['one', 'two', 'three', 'four']});
 });
 
 app.listen(port, () => {
