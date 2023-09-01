728203243df9b6f57d06d7cbcf4d9608db50731e
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-09-01 23:20:57 +0200

    testing git actions

--- backend_app/src/server.ts
+++ backend_app/src/server.ts
@@ -9,7 +9,7 @@ app.use(cors({
 }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.send("Hello from Kubernetes, World!");
   // res.json({"users": ['one', 'two', 'three', 'four']});
 });
 
